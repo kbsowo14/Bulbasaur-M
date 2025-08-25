0b7a95003f05097dc3699a9b80a4a8626dda7c94
Nongeoking <kbsowo14@naver.com>
2025-08-25 18:30:21 +0900

    Install tailwindcss & prettier

---
+++ src/components/atoms/CommText.tsx
@@ -0,0 +1,29 @@
+import { Text, TextProps } from 'react-native'
+import { memo, ReactNode } from 'react'
+
+interface CommTextProps extends TextProps {
+	children: ReactNode
+	minFontSize?: number
+	maxFontSize?: number
+}
+
+const CommText = ({
+	children,
+	minFontSize = 1,
+	maxFontSize = 1.2, // 사용자 설정 폰트사이즈에 레이아웃 깨짐 영향이 심한 경우, 0 사용
+	...args
+}: CommTextProps) => {
+	return (
+		<Text
+			allowFontScaling={false}
+			minimumFontScale={minFontSize}
+			maxFontSizeMultiplier={maxFontSize}
+			className="text-gray-800 text-sm font-sd-regular leading-6"
+			{...args}
+		>
+			{children}
+		</Text>
+	)
+}
+
+export default memo(CommText)
--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,24 +1,17 @@
-import { Button, Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+import CommText from 'components/atoms/CommText'
+import { TouchableOpacity, View } from 'react-native'
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
-      <Button screen="Profile" params={{ user: 'jane' }}>
-        Go to Profile
-      </Button>
-      <Button screen="Settings">Go to Settings</Button>
-    </View>
-  );
+	const navigation = useNavigation()
+	return (
+		<View className="flex-1 items-center justify-center">
+			<TouchableOpacity
+				className="rounded-full bg-blue-500 py-2 px-4"
+				onPress={() => navigation.navigate('Profile', { user: 'jane' })}
+			>
+				<CommText className="text-white">Go to jane's profile</CommText>
+			</TouchableOpacity>
+		</View>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10,
-  },
-});
